28acdacfabd6ee3cea8b4bd75200a01508c41883
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-23 05:33:12 +0000

    Modified code to check vertically and add empty array catch and tests.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,27 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['X', 'B', 'L', 'K', 'F', 'S', 'U', 'A'],
+      ['Y', 'F', 'C', 'F', 'Q', 'E', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'I', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'N', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'F', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'E', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'L', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'D', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return undefined if the array is present empty", function() {
+    const result = wordSearch([], 'SEINFELD')
+
+    assert.strictEqual(result, undefined);
   });
 });
